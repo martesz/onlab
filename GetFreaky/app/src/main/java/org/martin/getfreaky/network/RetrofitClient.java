3ed799f257aa5257abef8bf41ce5d2f59b234fe8
martesz <125exc@gmail.com>
2016-10-23 15:22:58 +0200

    Android client now uses TLS
    Android Client configured to use TLS, even if the Service does not have
    a certificate.

--- GetFreaky/app/src/main/java/org/martin/getfreaky/network/RetrofitClient.java
+++ GetFreaky/app/src/main/java/org/martin/getfreaky/network/RetrofitClient.java
@@ -4,6 +4,15 @@ package org.martin.getfreaky.network;
 import org.martin.getfreaky.GlobalVariables;
 
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
@@ -18,7 +27,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
  */
 public class RetrofitClient {
 
-    public static String BASE_URL = "http://192.168.1.2:20285/getFreakyService/getFreakyService/";
+    public static String BASE_URL = "https://192.168.1.2:20286/getFreakyService/getFreakyService/";
 
     private Retrofit retrofit;
 
@@ -28,7 +37,7 @@ public class RetrofitClient {
             @Override
             public Response intercept(Chain chain) throws IOException {
                 String token = application.getCurrentToken();
-                if(token == null) {
+                if (token == null) {
                     token = "not yet obtained";
                 }
 
@@ -41,7 +50,16 @@ public class RetrofitClient {
                 return chain.proceed(request);
             }
         });
-        OkHttpClient client = clientBuilder.build();
+        SSLSocketFactory sslSocketFactory = null;
+        try {
+            sslSocketFactory = getUnsafeSocketFactory();
+        } catch (KeyManagementException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        OkHttpClient client = clientBuilder
+                .sslSocketFactory(sslSocketFactory)
+                .hostnameVerifier(org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER)
+                .build();
 
         retrofit = new Retrofit.Builder()
                 .addConverterFactory(ScalarsConverterFactory.create())
@@ -57,5 +75,31 @@ public class RetrofitClient {
 
     public GetFreakyService createService() {
         return retrofit.create(GetFreakyService.class);
+    }
+
+    public SSLSocketFactory getUnsafeSocketFactory() throws KeyManagementException, NoSuchAlgorithmException {
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(
+                    java.security.cert.X509Certificate[] chain,
+                    String authType) throws CertificateException {
+            }
+
+            @Override
+            public void checkServerTrusted(
+                    java.security.cert.X509Certificate[] chain,
+                    String authType) throws CertificateException {
+            }
+
+            @Override
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new java.security.cert.X509Certificate[0];
+            }
+        }};
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, trustAllCerts, new SecureRandom());
+        SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+        return sslSocketFactory;
     }
 }
